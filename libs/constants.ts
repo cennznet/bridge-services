4c8de0930affb57473e6fe2a16bd33fdc4070312
aidan-starke <starke.aidan@gmail.com>
2022-05-26 14:47:00 +1200

    update constants

--- libs/constants.ts
+++ libs/constants.ts
@@ -9,7 +9,7 @@ export const RABBBITMQ_SERVER: string =
 	process.env.RABBBITMQ_SERVER ?? "amqp://guest:guest@localhost:5672";
 export const CENNZNET_NETWORK: string = process.env.CENNZNET_NETWORK ?? "local";
 export const ETHEREUM_NETWORK: string = process.env.ETHEREUM_NETWORK ?? "kovan";
-export const CENNZNET_SIGNER: string = process.env.CENNZNET_SIGNER ?? "";
+export const CENNZNET_SIGNER: string = process.env.CENNZNET_SIGNER ?? "//Alice";
 export const MESSAGE_MAX_RETRY: number = Number(
 	process.env.MESSAGE_MAX_RETRY ?? 3
 );
