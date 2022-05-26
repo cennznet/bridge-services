b9363b472b4ce39bd389cc072201476d9d124fe7
aidan-starke <starke.aidan@gmail.com>
2022-05-27 09:17:31 +1200

    remove unused types

--- libs/utils/getCENNZnetApi.ts
+++ libs/utils/getCENNZnetApi.ts
@@ -5,13 +5,5 @@ import { CENNZNetNetwork } from "@cennznet/api/types";
 export const getCENNZnetApi = async (): Promise<Api> => {
 	return await Api.create({
 		network: CENNZNET_NETWORK as CENNZNetNetwork,
-		types: {
-			ReturnDataClaim: {
-				_enum: {
-					Ok: "[u8; 32]",
-					ExceedsLengthLimit: null,
-				},
-			},
-		},
 	});
 };
