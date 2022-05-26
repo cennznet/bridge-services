ebbf76750b79253d968935bbedd2d00eebea1b95
aidan-starke <starke.aidan@gmail.com>
2022-05-27 09:04:54 +1200

    remove `MockProvider`

--- libs/utils/getEthersProvider.ts
+++ libs/utils/getEthersProvider.ts
@@ -6,11 +6,8 @@ import {
 	INFURA_PROJECT,
 } from "@/libs/constants";
 import { getDefaultProvider } from "ethers";
-import { MockProvider } from "ethereum-waffle";
 
 export const getEthersProvider = (): BaseProvider => {
-	if (ETHEREUM_NETWORK === "local") return new MockProvider();
-
 	return getDefaultProvider(ETHEREUM_NETWORK, {
 		alchemy: ALCHEMY_API_TOKEN,
 		infura: INFURA_PROJECT,
