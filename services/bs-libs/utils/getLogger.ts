9bf6c81b77484fd1c1a927bd1ffe2f889aed3c70
aidan-starke <starke.aidan@gmail.com>
2022-06-07 09:00:28 +1200

    remove colorization for log file

--- services/bs-libs/utils/getLogger.ts
+++ services/bs-libs/utils/getLogger.ts
@@ -19,38 +19,59 @@ const labels = {
 	EthDepositPoller: cennzPurple("EthDepositPoller"),
 };
 
-export function getLogger(service: LoggerService): Logger {
+export const getLogger = (service: LoggerService): Logger => {
 	if (instances[service]) return instances[service];
 
 	return (instances[service] = createLogger({
 		level: "info",
-		format: format.combine(
-			format.label({
-				label: labels[service],
-				message: true,
-			}),
-			format.timestamp({
-				format: "YYYY-MM-DD HH:mm:ss",
-			}),
-			format.errors({ stack: true }),
-			format.splat(),
-			format.json()
-		),
 		transports: [
 			new transports.File({
 				filename: `logs/app.log`,
 				maxsize: 2048000, // 2 MB
 				maxFiles: 10,
+				format: format.combine(
+					format.uncolorize(),
+					...getDefaultFormat(service)
+				),
 			}),
 
 			new transports.Console({
-				format: format.combine(
-					format.colorize(),
-					format.printf(({ level, message, timestamp }) => {
-						return `${timestamp} ${level}: ${message}`;
-					})
-				),
+				format: format.combine(format.colorize(), ...getColorized(service)),
 			}),
 		],
 	}));
+};
+
+function getDefaultFormat(service: string) {
+	return [
+		format.label({
+			label: service,
+			message: true,
+		}),
+		format.timestamp({
+			format: "YYYY-MM-DD HH:mm:ss",
+		}),
+		format.errors({ stack: true }),
+		format.splat(),
+		format.printf(({ level, message, timestamp }) => {
+			return `${timestamp} ${level}: ${message}`;
+		}),
+	];
+}
+
+function getColorized(service: LoggerService) {
+	return [
+		format.label({
+			label: labels[service],
+			message: true,
+		}),
+		format.timestamp({
+			format: "YYYY-MM-DD HH:mm:ss",
+		}),
+		format.errors({ stack: true }),
+		format.splat(),
+		format.printf(({ level, message, timestamp }) => {
+			return `${timestamp} ${level}: ${message}`;
+		}),
+	];
 }
