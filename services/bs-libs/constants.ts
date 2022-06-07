4a542928ce4e48f114ee5cf5f246569a9345e653
aidan-starke <starke.aidan@gmail.com>
2022-06-08 08:46:22 +1200

    use `||`, add `MESSAGE_MAX_TIME`

--- services/bs-libs/constants.ts
+++ services/bs-libs/constants.ts
@@ -4,12 +4,12 @@ import type { NetworkDetails } from "@bs-libs/types";
 import "dotenv/config";
 
 export const MONGODB_SERVER: string =
-	process.env.MONGODB_SERVER ?? "mongodb://root:root@localhost:27017/bridgeDb";
+	process.env.MONGODB_SERVER || "mongodb://root:root@localhost:27017/bridgeDb";
 export const RABBBITMQ_SERVER: string =
-	process.env.RABBBITMQ_SERVER ?? "amqp://guest:guest@localhost:5672";
-export const CENNZNET_NETWORK: string = process.env.CENNZNET_NETWORK ?? "local";
-export const ETHEREUM_NETWORK: string = process.env.ETHEREUM_NETWORK ?? "kovan";
-export const CENNZNET_SIGNER: string = process.env.CENNZNET_SIGNER ?? "";
+	process.env.RABBBITMQ_SERVER || "amqp://guest:guest@localhost:5672";
+export const CENNZNET_NETWORK: string = process.env.CENNZNET_NETWORK || "local";
+export const ETHEREUM_NETWORK: string = process.env.ETHEREUM_NETWORK || "kovan";
+export const CENNZNET_SIGNER: string = process.env.CENNZNET_SIGNER || "";
 
 export const INFURA_PROJECT = process.env.INFURA_PROJECT_ID
 	? {
@@ -18,11 +18,11 @@ export const INFURA_PROJECT = process.env.INFURA_PROJECT_ID
 	  }
 	: null;
 
-export const ALCHEMY_API_TOKEN: string = process.env.ALCHEMY_API_TOKEN ?? "";
+export const ALCHEMY_API_TOKEN: string = process.env.ALCHEMY_API_TOKEN || "";
 
-export const ETHERSCAN_API_TOKEN = process.env.ETHERSCAN_API_TOKEN ?? "";
+export const ETHERSCAN_API_TOKEN = process.env.ETHERSCAN_API_TOKEN || "";
 
-export const SLACK_SECRET: string = process.env.SLACK_SECRET ?? "";
+export const SLACK_SECRET: string = process.env.SLACK_SECRET || "";
 
 export const NETWORKS: Record<CENNZNetNetwork, NetworkDetails> = {
 	azalea: {
@@ -45,3 +45,5 @@ export const NETWORKS: Record<CENNZNetNetwork, NetworkDetails> = {
 
 export const NETWORK_DETAILS: NetworkDetails =
 	NETWORKS[CENNZNET_NETWORK as CENNZNetNetwork];
+
+export const MESSAGE_MAX_TIME = Number(process.env.MESSAGE_MAX_TIME || 30000)
